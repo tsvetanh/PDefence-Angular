d0773d816ea4ef3ec32e89cdceccc6a65c75c13e
tsvetanh <tsvetan.v.handzhiev@gmail.com>
2021-08-05 18:33:53 +0300

    Initial commit

---
+++ src/app/model/user.ts
@@ -0,0 +1,11 @@
+export class User {
+  id: string | undefined;
+  name: string | undefined;
+  email: string | undefined;
+  password: string | undefined;
+  rePassword: string | undefined;
+
+
+  constructor() {
+  }
+}
