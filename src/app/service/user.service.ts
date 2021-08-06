e217ce387fd00c2fe0541ffe2eb1e8d6f8aab5bc
tsvetanh <tsvetan.v.handzhiev@gmail.com>
2021-08-06 08:44:42 +0300

    enviorment url

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,25 +2,27 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../model/user";
+import {environment} from "../../environments/environment";
+
+  const usersUrl = environment.api;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private usersUrl: string;
 
   constructor(private http: HttpClient) {
-    this.usersUrl = 'https://project-defend.herokuapp.com/users';
+    // this.usersUrl = 'https://project-defend.herokuapp.com/users';
   }
 
   public findAll(): Observable<User[]> {
-    return this.http.get<User[]>(this.usersUrl);
+    return this.http.get<User[]>(usersUrl + "/users");
   }
 
   public save(user: User) {
     console.log(user);
-    return this.http.post<User>(this.usersUrl, user);
+    return this.http.post<User>(usersUrl, user);
   }
 }
 
